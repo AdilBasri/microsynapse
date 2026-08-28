f6dc31f7967f61fe9969ba6ee8725ef1e6133198
Adil Basri ERDEM <adilbasri06161@gmail.com>
2026-08-28 12:46:49 +0300

    fix: update root package.json scripts for backend and AI, remove deprecated Mongoose options

--- subtracker-backend/middlewares/dbConnection.js
+++ subtracker-backend/middlewares/dbConnection.js
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const dbConnection = mongoose.createConnection(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const dbConnection = mongoose.createConnection(process.env.MONGO_URI);
 
 export default dbConnection;
